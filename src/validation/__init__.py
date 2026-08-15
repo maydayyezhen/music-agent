53d672fd13c95b93441c14362a1dd4b14707bd4d
Zhang Zihao <162564657+maydayyezhen@users.noreply.github.com>
2026-08-15 19:11:22 +0800

    export generic melody structure validator

--- src/validation/__init__.py
+++ src/validation/__init__.py
@@ -2,5 +2,12 @@ from .instrument_aware import analyze_instrument_aware
 from .strumming import analyze_strumming_flow
 from .long_form_phrase_validator import analyze_long_form_phrases
 from .melody_skeleton import validate_melody_skeleton
+from .melody_structure import analyze_melody_structure
 
-__all__ = ["analyze_instrument_aware", "analyze_long_form_phrases", "validate_melody_skeleton", "analyze_strumming_flow"]
+__all__ = [
+    "analyze_instrument_aware",
+    "analyze_long_form_phrases",
+    "validate_melody_skeleton",
+    "analyze_melody_structure",
+    "analyze_strumming_flow",
+]
